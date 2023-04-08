9231b7434047473628482ba33268ca73a2f799d1
gmgunesoglu <76867018+gmgunesoglu@users.noreply.github.com>
2023-04-08 11:41:28 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String giris = "(1+(7+(8*2-4))/23)*2/(1+(7+(8*2))/23)";
+        String test1= "8+2*(7+(8*2))/23/2/8*2+10";
+        String test2="(8*2)/23/2";
+        String test3="(2*60+7/3)/7";
+        String test4="7+(8*2-4)/3*2";
+
+        IslemAgaci islemAgaci=new IslemAgaci(test4);
+        islemAgaci.dallan();
+        islemAgaci.solKokSag();
+        System.out.println("islem sonucu = "+islemAgaci.isle());
     }
 }
